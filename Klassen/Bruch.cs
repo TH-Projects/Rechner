ecd0b29325dfa19d5c66a117b935a26965f2feef
TH-Projects <64643362+TH-Projects@users.noreply.github.com>
2020-05-26 13:26:53 +0200

    Initial Commit

---
+++ Klassen/Bruch.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rechner
+{
+	class Bruch
+	{
+		int Nenner = 1;
+		int Zaeler = 1;
+		public Bruch(int Zaeler, int Nenner)
+		{
+			if (Nenner != 0)
+			{
+				this.Zaeler = Zaeler;
+				this.Nenner = Nenner;
+			}
+			else
+			{
+				Console.WriteLine("Fehler");
+			}
+		}
+		public void Print()
+		{
+			Console.WriteLine("{0}/{1}", Zaeler, Nenner);
+		}
+		public void AddBruch(Bruch bruch2)
+		{
+			if (Nenner == bruch2.Nenner)
+			{
+				Zaeler += bruch2.Zaeler;
+			}
+			else
+			{
+				int tempnenn = Nenner;
+				Nenner *= bruch2.Nenner;
+				Zaeler *= bruch2.Nenner;
+				bruch2.Nenner *= tempnenn;
+				bruch2.Zaeler *= tempnenn;
+				Zaeler += bruch2.Zaeler;
+			}
+		}
+		public void SubBruch(Bruch bruch2)
+		{
+			if (Nenner == bruch2.Nenner)
+			{
+				Zaeler -= bruch2.Zaeler;
+			}
+			else
+			{
+				int tempnenn = Nenner;
+				Nenner *= bruch2.Nenner;
+				Zaeler *= bruch2.Nenner;
+				bruch2.Nenner *= tempnenn;
+				bruch2.Zaeler *= tempnenn;
+				Zaeler -= bruch2.Zaeler;
+			}
+		}
+		public void Multibruch(Bruch bruch2)
+		{
+			Nenner *= bruch2.Nenner;
+			Zaeler *= bruch2.Zaeler;
+		}
+		public void DivBruch(Bruch bruch2)
+		{
+			int tempzael = bruch2.Zaeler;
+			bruch2.Zaeler = bruch2.Nenner;
+			bruch2.Nenner = tempzael;
+			Nenner *= bruch2.Nenner;
+			Zaeler *= bruch2.Zaeler;
+		}
+	}
+}
---
+++ Klassen/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Rechner
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] WBruch = new int[4];
+			WBruch = BenBruch();
+			
+			Bruch bruch = new Bruch(WBruch[0], WBruch[1]);
+			Bruch bruch2 = new Bruch(WBruch[2], WBruch[3]);
+			Console.Write("Geben sie ihren Rechenoperator ein:(+,-,*,/) \n");
+			switch (Convert.ToChar(Console.ReadLine()))
+			{
+				case '+':
+					bruch.AddBruch(bruch2);
+					bruch.Print();
+					break;
+				case '-':
+					bruch.SubBruch(bruch2);
+					bruch.Print();
+					break;
+				case '*':
+					bruch.Multibruch(bruch2);
+					bruch.Print();
+					break;
+				case '/':
+					bruch.DivBruch(bruch2);
+					bruch.Print();
+					break;
+				default:
+					Console.WriteLine("Fehlerhafter Opperator!");
+					break;
+			}
+			Console.ReadKey();
+		} 
+		static int[] BenBruch()
+		{
+			int[] temp = new int[4];
+			Console.Write("Bruchrechnung\nGeben sie den Zaeler des ersten Bruches ein: \n");
+			temp[0] = Convert.ToInt32(Console.ReadLine());
+			Console.Write("Geben sie den Nenner des ersten Bruches ein: \n");
+			temp[1] = Convert.ToInt32(Console.ReadLine());
+			Console.Write("Geben sie den Zaeler des zweiten Bruches ein: \n");
+			temp[2] = Convert.ToInt32(Console.ReadLine());
+			Console.Write("Geben sie den Nenner des zweiten Bruches ein: \n");
+			temp[3] = Convert.ToInt32(Console.ReadLine());
+			return temp;
+		}
+	} 
+}
