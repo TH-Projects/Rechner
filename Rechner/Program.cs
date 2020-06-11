77977fe71c5d030105cad0c1a54e9100661d80c0
TH-Projects <64643362+TH-Projects@users.noreply.github.com>
2020-06-11 15:25:23 +0200

    Renamed files and folder
    Fixes #2

---
+++ Rechner/Program.cs
@@ -0,0 +1,199 @@
+﻿using System;
+
+
+namespace Rechner
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Program programm = new Program();
+			double Zahl1 = 0;
+			double Zahl2 = 0;
+			//Erstellung des Objekts UserInput
+			UserInput userInput = new UserInput();
+			
+			//Abfrage der Rechnungsart
+			int Decision = userInput.UserOption();
+			switch (Decision)
+			{
+				//Auswahl: Bruchrechnen
+				case 1:
+					//Erstellung Bruch eins
+					Bruch bruch = userInput.UserBruch();
+					//Erstellung Bruch zwei
+					Bruch bruch2 = userInput.UserBruch();
+					//Aufruf Methode AuswBruch
+					AuswBruch(bruch, bruch2);
+					break;
+
+				//Auswahl: Grundrechnung
+				case 2:
+					//Zuweisung der Zahlen eins und zwei
+					Zahl1 = userInput.UserGrund();
+					Zahl2 = userInput.UserGrund();
+					//Aufruf Methode AuswGrund
+					AuswGrund(Zahl1, Zahl2);
+					break;
+			}
+			programm.Nochmal();
+		} 
+		static void AuswBruch(Bruch bruch, Bruch bruch2)
+		{
+			//Ausgabe Frage zu Opperator
+			Console.Write("Geben sie ihren Rechenoperator ein:(+,-,*,/) \n");
+			switch (Convert.ToChar(Console.ReadLine()))
+			{
+				case '+':
+					//Aufruf der Bruchaddition
+					bruch.AddBruch(bruch2);
+					//Ausgabe Bruch
+					bruch.Print();
+					break;
+				case '-':
+					//Aufruf der Bruchsubtraktion
+					bruch.SubBruch(bruch2);
+					//Ausgabe Bruch
+					bruch.Print();
+					break;
+				case '*':
+					//Aufruf der Bruchmultiplikation
+					bruch.Multibruch(bruch2);
+					//Ausgabe Bruch
+					bruch.Print();
+					break;
+				case '/':
+					//Aufruf der Bruchdivision
+					bruch.DivBruch(bruch2);
+					//Ausgabe Bruch
+					bruch.Print();
+					break;
+				default:
+					//Wenn falscher Opperator
+					Console.WriteLine("Fehlerhafter Opperator!");
+					break;
+			}
+		}
+		static void AuswGrund(double Zahl1,double Zahl2)
+		{
+			//Erstellung des Objekts Rechnen
+			Grundrechenarten Rechnen = new Grundrechenarten(Zahl1, Zahl2);
+			//Ausgabe Frage nach Opperator
+			Console.Write("Geben sie ihren Rechenoperator ein:(+,-,*,/) \n");
+			switch (Convert.ToChar(Console.ReadLine()))
+			{
+				case '+':
+					//Aufruf der Addition mit Ausgabe
+					Console.WriteLine(Rechnen.Addition(Zahl1,Zahl2));
+					break;
+				case '-':
+					//Aufruf der Subtraktion mit Ausgabe
+					Console.WriteLine(Rechnen.Subtraktion(Zahl1, Zahl2));
+					break;
+				case '*':
+					//Aufruf der Multiplikation mit Ausgabe
+					Console.WriteLine(Rechnen.Multiplikation(Zahl1, Zahl2));
+					break;
+				case '/':
+					//Aufruf der Division mit Ausgabe
+					Console.WriteLine(Rechnen.Division(Zahl1, Zahl2));
+					break;
+				default:
+					//Wenn falscher Opperator
+					Console.WriteLine("Fehlerhafter Opperator!");
+					break;
+			}
+		}
+		//Abfrage ob nochmal gerechnet werden soll
+		public void Nochmal()
+		{
+			//Erstellung Objekt von der Klasse UserInput
+			UserInput userInput = new UserInput();
+			//Erstellung eines string Arrays zum Aufruf der Program.Main()
+			string[] bla = new string[1];
+			//Zuweisung der Benutzereingabe zu einer Variablen
+			int antw = userInput.Repeat();
+			
+			switch (antw)
+			{
+				//Wenn eins, wird die Main-Methode wiederholt
+				case 1:
+					Main(bla);
+					break;
+				//Wenn zwei beendigung des Programms
+				case 2:
+					Environment.Exit(1);
+					break;
+				//Bei Fehler wird Abfrage wiederholt
+				default:
+					Console.WriteLine("Fehler!");
+					//Erneuter Aufruf der Nochmal()-Methode
+					Nochmal();
+					break;
+			}
+		}
+	}
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Dieses Programm wurde für gehirnamputierte Affen kommentiert
---
+++ Rechner/UserInput.cs
@@ -0,0 +1,166 @@
+﻿using System;
+
+
+namespace Rechner
+{
+	class UserInput
+	{
+		public UserInput()
+		{
+		}
+
+		//Auswahl was gerrechnet werden soll
+		public int UserOption()
+		{
+			int Eingabe;
+			try
+			{
+				//Frage nach Eingabe
+				Console.WriteLine("Was wollen sie Rechnen?\nBruchrechnen(1)\nRechnen mit Grundrechenarten(2) ");
+				//Zuweisung der Eingabe zu Variable
+				Eingabe = Convert.ToInt32(Console.ReadKey().KeyChar.ToString());
+				//Abwarten
+				Console.ReadKey();
+				//Konsole berreinigen
+				Console.Clear();
+				//Rückgabe der Eingabe
+				return Eingabe;
+			}
+			//Exeption handling, wenn Format-Exception aufritt
+			catch (FormatException)
+			{
+				FormExMessage();
+				return 0;
+			}
+			//Exception handling, falls andere Exception
+			catch (Exception)
+			{
+				ExMessage();
+				return 0;
+			}
+		}
+
+		//Benutzereingabe des Bruches
+		public Bruch UserBruch()
+		{
+			try
+			{
+				//Erstellung eines Arrays zum speichern der Bruchwerte
+				int[] temp = new int[2];
+				//Eingabeaufforderung für den Zähler
+				Console.Write("Geben sie den Zaeler des Bruches ein: \n");
+				//Zuweisung zu Array index 0
+				temp[0] = Convert.ToInt32(Console.ReadLine());
+				//Eigabeaufforderung für den Nenner
+				Console.Write("Geben sie den Nenner des Bruches ein: \n");
+				//Zuweisung zu Array index 1
+				temp[1] = Convert.ToInt32(Console.ReadLine());
+				//Rückgabe des Arrays
+				return CreateBruch(temp);
+			}
+			//Exeption handling, wenn Format-Exception aufritt
+			catch (FormatException)
+			{
+				FormExMessage();
+				return null;
+			}
+			//Exception handling, falls andere Exception
+			catch (Exception)
+			{
+				ExMessage();
+				return null;
+			}
+		}
+		//Erstellung des Bruches
+		private Bruch CreateBruch(int[] temp)
+		{
+			//Erstellung des Bruches
+			Bruch bruch = new Bruch(temp[0], temp[1]);
+			//Rüchgabe des Bruches an Program.cs
+			return bruch;
+		}
+		//Benutzereingabe für Grundrechnung
+		public double UserGrund()
+		{
+			try
+			{
+				double Zahl;
+				//Eingabeaufforderung
+				Console.Write("Geben sie ihre Zahl ein!: \n");
+				//Zuweisung der Eingabe zu Variable
+				Zahl = Convert.ToDouble(Console.ReadLine());
+				//Rückgabe Zahl
+				return Zahl;
+			}
+			//Exeption handling, wenn Format-Exception aufritt
+			catch (FormatException)
+			{
+				FormExMessage();
+				return 0;
+			}
+			//Exception handling, falls andere Exception
+			catch (Exception)
+			{
+				ExMessage();
+				return 0;
+			}
+		}
+		public int Repeat()
+		{
+			//Deklariation von Variable für Benutzereingabe
+			char Eingabe = ' ';
+			//Ausgabe Frage
+			Console.WriteLine("Wollen sie noch mehr rechnen? Y/N");
+			//Zuweisung der Antwort zu Variable
+			Eingabe = Convert.ToChar(Console.ReadLine());
+			try
+			{
+				//Bei Eingabe y/Y wird 1 returnt
+				if (Eingabe == 'y' || Eingabe == 'Y')
+				{
+					return 1;
+				}
+				//Bei Eingabe n/N wird 2 returnt
+				else if (Eingabe == 'n' || Eingabe == 'N')
+				{
+					return 2;
+				}
+			}
+			//Allgemeines Exeption handling
+			catch (Exception)
+			{
+				//Aufrug Methode ExMessage()
+				ExMessage();
+			}
+			return 0;
+		}
+
+		//Nachricht bei Format-Exception
+		public void FormExMessage()
+		{
+			//Output Nachricht
+			Console.WriteLine("Es liegt ein Fehler bei der Eingabe vor!\nBitte starten sie das Programm erneut!");
+			Console.ReadKey();
+			//Beendigung des Programms
+			Environment.Exit(1);
+		}
+		//Nachricht bei DivisionbyZero Exception
+		public void DivZeroExMessage()
+		{
+			//Ausgabe Nachricht
+			Console.WriteLine("Es kann nicht durch 0 geteilt werden!\nBitte starten sie das Programm erneut!");
+			Console.ReadKey();
+			//Beendingung des Programms
+			Environment.Exit(1);
+		}
+		//Nachricht bei anderer Exception
+		public void ExMessage()
+		{
+			//Ausgabe Message
+			Console.WriteLine("Fehler! Bitte starten sie das Programm neu.");
+			Console.ReadKey();
+			//Beendigung des Programms
+			Environment.Exit(1);
+		}
+	}
+}
